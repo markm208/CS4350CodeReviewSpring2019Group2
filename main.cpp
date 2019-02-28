24bc2aa8dc8d818577b45be8684d74306b053510
andrew-heneghan <andrew.heneghan@att.net>
2019-02-28 15:02:01 -0600

    Convert both fractions to improper
    Both fractions have their correct signs.  All I need to do now is simplify the answer and do long division to be placed in the result c string

--- main.cpp
+++ main.cpp
@@ -2,9 +2,23 @@
 
 using namespace std;
 
+struct Fraction{
+    char sign;
+    int constant;
+    int numerator;
+    int denominator;
+};
 
 bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
 int findLCM(int d1, int d2);
+bool isNegative(int c, int n, int d);
+int convertToImproper(Fraction fraction); //--> Would probably have to return a c string.... 12,10 where 12 is the numerator/10 is denominator
+void freeAndNull(void **ptr);
+int abs(int num);
+void printFractions(Fraction firstFraction);
+Fraction addFractions(Fraction firstFraction, Fraction secondFraction); 
+Fraction changeSigns(Fraction fraction);
+
 
 int main()
 {
@@ -13,57 +27,176 @@ int main()
     int c1, n1, d1;
     int c2, n2, d2; 
     
-
+    cout << "1******************" << endl;
     c1 = 1;
-    n1 = 1;
-    d1 = 2;
+    n1 = 2;
+    d1 = 6;
  
-    c2 = 2;
-    n2 = 2;
-    d2 = 3; 
+    c2 = 0;
+    n2 = 5;
+    d2 = 1; 
 
     add(c1,n1,d1,c2,n2,d2,answer,10);
+    cout << endl;
+    cout << "2*****************" << endl;
 
-    c1 = -5;
-    n1 = 6;
-    d1 = 360;
+    c1 = -1;
+    n1 = 2;
+    d1 = 6;
  
-    c2 = 2;
-    n2 = 2;
-    d2 = 245; 
+    c2 = 0;
+    n2 = 5;
+    d2 = 1; 
 
     add(c1,n1,d1,c2,n2,d2,answer,10);
-
-    c1 = -5;
-    n1 = 6;
-    d1 = 7;
+    cout << endl;
+    cout << "3*****************" << endl;
+    c1 = 1;
+    n1 = 2;
+    d1 = 6;
  
-    c2 = 2;
-    n2 = 2;
-    d2 = 1246; 
+    c2 = -0;
+    n2 = -5;
+    d2 = 1; 
 
     add(c1,n1,d1,c2,n2,d2,answer,10);
+    cout << endl;
+    cout << "4*****************" << endl;
 
+    c1 = -1;
+    n1 = 2;
+    d1 = 6;
+ 
+    c2 = 0;
+    n2 = -5;
+    d2 = 1; 
+
+    add(c1,n1,d1,c2,n2,d2,answer,10);
+    cout << endl;
+    cout << "5*****************" << endl;
 
     return 0;
- 
 }
 
 //new functions go here
 bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
 {
+    //Local variables
+    Fraction firstFraction;
+    Fraction secondFraction;
+    Fraction answer;
+
     bool retVal;
     int lcm;
+    if(d1 == 0 || d2 == 0)
+    {
+        retVal = false;
+        return retVal;
+    }
 
+    firstFraction.constant = c1;
+    firstFraction.numerator = n1;
+    firstFraction.denominator = d1;
 
+    secondFraction.constant = c2;
+    secondFraction.numerator = n2;
+    secondFraction.denominator = d2;
+    
 
-    //Check should check to see if chara
+    //AT THIS POINT, EACH INDIVIDUAL FRACTION HAS THEIR CORRECT SIGN and IS PARSED
+
+    //Convert to improper
+   firstFraction.numerator = convertToImproper(firstFraction);
+   firstFraction.constant = '\0';
+   
+   secondFraction.numerator = convertToImproper(secondFraction);
+   secondFraction.constant = '\0';
+
+   //Then get the sign
+   firstFraction = changeSigns(firstFraction);
+   secondFraction = changeSigns(secondFraction);
+
+    cout << endl;
+    cout << "SIGNS SHOULD BE CORRECT" << endl;
+    printFractions(firstFraction);
+    printFractions(secondFraction);
+    cout << endl;
 
     lcm = findLCM(d1,d2);
+    //What to multiply the fraction by.
+
+    if(firstFraction.denominator != lcm)
+    {
+        int multiplier = lcm/firstFraction.denominator;
+    
+        firstFraction.denominator = firstFraction.denominator * multiplier;
+        firstFraction.numerator = firstFraction.numerator * multiplier;
+    }
+
+    if(secondFraction.denominator != lcm)
+    {
+        int multiplier = lcm/secondFraction.denominator;
+
+        secondFraction.denominator = secondFraction.denominator * multiplier;
+        secondFraction.numerator = secondFraction.numerator * multiplier;
+    }
+    //All fractions should now be imporoper and have correct LCM
+    cout << endl;
+    printFractions(firstFraction);
+    printFractions(secondFraction);
+    cout << endl;
+
+    answer = addFractions(firstFraction, secondFraction);
+    //Add
+    cout << endl;
+    printFractions(answer);
+    cout << endl;
+    //Mod final result
+
+    //Long division
+
+    //Fill result[]
 
 
+
+
+    //Buffers used for helper functions
+    char *improperConversionPtr = (char *)malloc(len);
+
+ //   convertToImproper(improperConversionPtr, c1, n1, d1, len);
+
+    freeAndNull((void**)improperConversionPtr);
 
     return retVal;
+}
+int convertToImproper(Fraction fraction) //--> Would probably have to return a c string.... 12,10 where 12 is the numerator/10 is denominator
+{
+    int newNumerator;
+    newNumerator =  (fraction.denominator * fraction.constant) + fraction.numerator;
+
+    return newNumerator;
+}
+void freeAndNull(void **ptr)
+{
+   free(*ptr);
+   *ptr = NULL;
+}
+int abs(int v) 
+{
+  return v * ( (v<0) * (-1) + (v>0));
+  // simpler: v * ((v>0) - (v<0))   thanks Jens
+}
+void printFractions(Fraction firstFraction)
+{
+    if(firstFraction.sign == '+')
+    {
+        cout << "Fraction: " << firstFraction.constant << " " << firstFraction.numerator << "/" << firstFraction.denominator << endl;
+        //print without
+    }
+    else
+    {
+        cout << "Fraction: " << firstFraction.sign << firstFraction.constant << " " << firstFraction.numerator << "/" << firstFraction.denominator << endl;
+    }
 }
 int findLCM(int d1, int d2)
 {
@@ -87,4 +220,43 @@ int findLCM(int d1, int d2)
         lcm++;
     }
     return lcm;
+}
+Fraction changeSigns(Fraction fraction)
+{
+    printFractions(fraction);
+    Fraction correctSigns;
+    //if both are numerator and denominator are less than zero, abs on bottom
+    if((fraction.numerator < 0 && fraction.denominator < 0) ||
+    (fraction.numerator > 0 && fraction.denominator > 0))
+    {
+        correctSigns.numerator = abs(fraction.numerator);
+        correctSigns.denominator = abs(fraction.denominator);
+        correctSigns.sign = '+';
+    }
+    else
+    {
+        //Fraction is negative...make numerator negative, denominator positive, sign for fraction negative
+        correctSigns.numerator = abs(fraction.numerator);
+        correctSigns.numerator *= -1;
+        correctSigns.denominator = abs(fraction.denominator);
+        correctSigns.sign = '-';
+    }
+
+    return correctSigns;
+}
+Fraction addFractions(Fraction firstFraction, Fraction secondFraction)
+{
+    Fraction answer; 
+    answer.denominator = secondFraction.denominator;
+    answer.numerator = firstFraction.numerator + secondFraction.numerator;
+
+    if(answer.numerator <= -0)
+    {
+        answer.sign = '-';
+    }
+    else
+        answer.sign = '+';
+
+    return answer;
+
 }
