a3a195854c84c4dfc759cf7712283d96de53ace7
Michael Bisciglia <mbisciglia@carthage.edu>
2019-03-11 17:07:26 -0500

    Reworked Characteristic
    Some of the main complaints people had were about having to reiderate through the char[] more times than neeeded. I agreed, and have made it so my program will never iterate more than once. Another issue was that there was unneeded confusion with allocating new data for the cleaned version of the string, and so I scrapped all of that and did everything inside of the characteristic function. This was done as a test, but I think it reduces confusion. Another major change was including the process of building the integer while checking the validity of the char []. I also included a possible issue where input is given but would overflow integer max if computed.

--- main.cpp
+++ main.cpp
@@ -2,156 +2,78 @@
 
 using namespace std;
 
+
+#define ASCII_0 48
+#define ASCII_9 57
+
 bool characteristic(char numString[], int& c);
-int getStringSize(char numString[]);
-void getFirstHalf(char originalString[], char newString[]);
-bool validateNum(char * newString);
-int arrToInt(char * validatedString);
 
 int main()
 {
 	char number[] = "  123.53245456";
 	int c = 0;
 
-	characteristic(number, c);
-	cout << c << endl;
+	if (characteristic(number, c))
+		cout << c << endl;
+	else
+		cout << "error with input string" << endl;
 
 	system("pause");
 	return 0;
 }
 
-//characteristic function takes in two parameters: numString is a char [] representation of a number,
-//int c is the int that is supposed to be represented in numString, the point of the function is so that c == numString as an int
-//if the data passed in is not valid, we must return false
+
+
 bool characteristic(char numString[], int& c)
 {
-	//bool variable avoids multiple return statements
 	bool isValid = true;
 
-	//dynamically allocated with +1 for '\0'
-	char * firstHalf = new char[getStringSize(numString) + 1]{};
-
-	//firstHalf will hold all of the elements before the '.' while removing spaces
-	getFirstHalf(numString, firstHalf);
-
-	//validateNum will make sure there are no bad symbols, make sure the first element is + or -, and will return true if valid 
-	if (!validateNum(firstHalf))
-	{
-		cout << "User input is not valid on left side of '.'" << endl;
-		isValid = false;
-	}
-
-	//c gets the value returned from conversion function
-	c = arrToInt(firstHalf);
-
-	//Get that Memory back, it's not like we can just download more RAM ;)
-	delete firstHalf;
-	return isValid;
-}
-
-//this function was implemented to allow for dynamic allocation of the char []
-int getStringSize(char numString[])
-{
-	char* currentAddress = numString;
-	int counter = 0;
-	for (currentAddress; *currentAddress != '\0'; currentAddress++) {
-		if (*currentAddress == '.')
-		{
-			break;
-		}
-		else if (*currentAddress == ' ')
-		{
-			continue;
-		}
-		counter++;
-	}
-	return counter;
-}
-
-//Having a seperate function to parse the correct part of the string allows for more readable code
-//numString is the original char[], firstHalf is the inheriting char[] that will only get good values before the '.'
-void getFirstHalf(char originalString[], char newString[])
-{
-	char* currentAddress = originalString;
-	int counter = 0;
-	for (currentAddress; *currentAddress != '\0'; currentAddress++) {
-		if (*currentAddress == '.')
-		{
-			break;
-		}
-		else if (*currentAddress == ' ')
-		{
-			continue;
-		}
-
-		newString[counter] = *currentAddress;
-		counter++;
-	}
-}
-
-//This function does the work for the characteristic boolean return, and checks the string for any bad inputs
-bool validateNum(char * newString)
-{
-	bool isStringValid = true;
-	char * currentAddress = newString;
-
-	//two differnt forms of input
-	//one where there could be '+' or '-' in first element
-	if (*currentAddress == '+' || *currentAddress == '-')
-	{
-		//increment if first element is '+' or '-'
-		currentAddress++;
-		for (currentAddress; *currentAddress != '\0'; currentAddress++) {
-			//ascii values 0-9
-			//will fall into if if char at *currentAddress != 0-9
-			if ((*currentAddress <= 48 || *currentAddress >= 57))
-			{
-				isStringValid = false;
-				break;
-			}
-		}
-	}
-	//and one where there is no '+' and no '-'
-	else
-	{
-		for (currentAddress; *currentAddress != '\0'; currentAddress++) {
-			//ascii values 0-9
-			//will fall into if if char at *currentAddress != 0-9
-			if ((*currentAddress <= 48 || *currentAddress >= 57))
-			{
-				isStringValid = false;
-				break;
-			}
-		}
-	}
-
-	return isStringValid;
-}
-
-//converts the validated string to int
-int arrToInt(char * validatedString)
-{
+	//True = positive, False = negative
+	bool sign = true;
+	//The running value of the characteristic
 	int res = 0;
-	char * currentAddress = validatedString;
-	bool isNegative = false;
 
-	if (*currentAddress == '-')
+	char * currentAddress = numString;
+
+	//skips over leading spacess
+	while (*currentAddress == ' ')
 	{
-		isNegative = true;
 		currentAddress++;
 	}
-	else if (*currentAddress == '+')
+
+	//checks if string begins with + or -
+	if (*currentAddress == '+')
 	{
+		sign = true;
+		currentAddress++;
+	}
+	else if (*currentAddress == '-')
+	{
+		sign = false;
 		currentAddress++;
 	}
 
 	for (currentAddress; *currentAddress != '\0'; currentAddress++) {
-		res = res * 10 + *currentAddress - '0';
-	}
-	if (isNegative)
-	{
-		res *= -1;
+		//End of characteristic
+		if (*currentAddress == '.')
+		{
+			break;
+		}
+		else if ((*currentAddress <= ASCII_0 || *currentAddress >= ASCII_9))
+		{
+			isValid = false;
+			break;
+		}
+		else
+		{
+			//Will continue to add to res and shift digits over by multiplying res by 10
+			res = res * 10 + *currentAddress - '0';
+		}
 	}
 
-	return res;
+	//if everything was good, place res into c
+	if (isValid)
+		c = res; 
+
+	return isValid;
 }
